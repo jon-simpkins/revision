895e129038576f6b98935b45a3a80f56ab7c4d04
Jon Simpkins <jon.simpkins@gmail.com>
2019-08-11 14:23:32 -0500

    feat: build out POC for fetching from docs api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import {credentials} from '../credentials';
+
+declare var gapi: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +12,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Revision';
 
-  signIn() {
-    console.log('Sign in goes here');
+  API_KEY = credentials.api_key;
+  CLIENT_ID = credentials.web.client_id;
+
+  initApi() {
+    const DISCOVERY_DOCS = [
+      'https://docs.googleapis.com/$discovery/rest?version=v1&key=' + this.API_KEY];
+
+    const SCOPES = "https://www.googleapis.com/auth/documents.readonly";
+
+    gapi.load('client:auth2', () => {
+      gapi.client.init({
+        apiKey: this.API_KEY,
+        clientId: this.CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+      });
+    });
   }
 
-  signOut() {
+  signIn() {
+    console.log('Sign in goes here');
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  fetchDoc() {
     console.log('Sign out goes here');
+    gapi.client.docs.documents.get({
+      documentId: '195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE'
+    }).then(function(response) {
+      console.log(response);
+    });
   }
 }
