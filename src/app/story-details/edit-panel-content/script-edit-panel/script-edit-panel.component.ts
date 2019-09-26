8fe9d2755729e38332cf71daebe7c3083d6552f0
Jon Simpkins <jon.simpkins@gmail.com>
2019-09-26 06:26:26 -0500

    feat: add basic word count, autofocus for script editor panel

--- src/app/story-details/edit-panel-content/script-edit-panel/script-edit-panel.component.ts
+++ src/app/story-details/edit-panel-content/script-edit-panel/script-edit-panel.component.ts
@@ -15,13 +15,26 @@ export class ScriptEditPanelComponent implements OnInit {
     toolbar: []
   };
 
+  wordCount = 0;
+
   constructor() {}
 
   ngOnInit() {
+    this.updateWordCount();
   }
 
   onQuillContentChanged(change) {
     // console.log(change);
     this.editContent.script.rawText = change.text;
+    this.updateWordCount();
   }
+
+  setFocus(editor) {
+    editor.focus();
+  }
+
+  updateWordCount() {
+    this.wordCount = this.editContent.script.rawText.split(/\w+/).length - 1;
+  }
+
 }
