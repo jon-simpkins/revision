60848a9bd70e52bc70ad4f6f16954ec51011a4c9
Jon Simpkins <jon.simpkins@gmail.com>
2019-12-15 10:19:47 -0600

    fix: restore the font

--- revision/lib/main.dart
+++ revision/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.green,
-        // fontFamily: 'CourierPrime'
+        fontFamily: 'CourierPrime'
       ),
       home: MyHomePage(title: 'Revision'),
     );
