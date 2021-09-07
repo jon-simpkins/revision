a2f72d9892c254d819949d66d4de118b3c7115ee
Jon Simpkins <jonsimpkins@google.com>
2021-09-07 09:47:40 -0500

    :construction: add semantic-ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { Button, Icon, Label } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
@@ -12,45 +13,18 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
+      <div className="Semantic-example">
+        <Button as='div' labelPosition='right'>
+          <Button icon>
+            <Icon name='heart' />
+            Like
+          </Button>
+          <Label as='a' basic pointing='left'>
+            2,048
+          </Label>
+        </Button>
+      </div>
     </div>
   );
 }
