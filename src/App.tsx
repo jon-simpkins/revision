35432b0d4275248f10307a47f6d206a50acea1fb
Jon Simpkins <jonsimpkins@google.com>
2021-09-07 15:13:52 -0500

    :construction: add router scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import { Button, Icon, Label } from 'semantic-ui-react'
 import './App.css';
 import RevisionHeader from './features/revision-header/RevisionHeader';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <RevisionHeader />
-      <div className="Semantic-example">
-        <Button as='div' labelPosition='right'>
-          <Button icon>
-            <Icon name='heart' />
-            Like
-          </Button>
-          <Label as='a' basic pointing='left'>
-            2,048
-          </Label>
-        </Button>
-      </div>
-    </div>
+      <Switch>
+        <Route path="/about">Writing!</Route>
+        <Route path="/">
+          <div className="Semantic-example">
+            <Button as='div' labelPosition='right'>
+              <Button icon>
+                <Icon name='heart' />
+                Like
+              </Button>
+              <Label as='a' basic pointing='left'>
+                2,048
+              </Label>
+            </Button>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
