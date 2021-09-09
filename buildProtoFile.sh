3c1cc20515b4539a549dcbc895a8ba23926a7c96
Jon Simpkins <jonsimpkins@google.com>
2021-09-08 20:08:20 -0500

    :building_construction: added new proto file for migration

--- buildProtoFile.sh
+++ buildProtoFile.sh
@@ -6,3 +6,12 @@ pbts -o src/protos.d.ts src/protos.js
 # Prepend with the necessary import
 echo "import Long = require('long');"|cat - src/protos.d.ts > tmp_protos_file
 mv tmp_protos_file src/protos.d.ts
+
+# Now the same, but for v2
+
+pbjs -t static-module -o src/protos_v2.js src/protos_v2.proto
+pbts -o src/protos_v2.d.ts src/protos_v2.js
+
+# Prepend with the necessary import
+echo "import Long = require('long');"|cat - src/protos_v2.d.ts > tmp_protos_v2_file
+mv tmp_protos_v2_file src/protos_v2.d.ts
