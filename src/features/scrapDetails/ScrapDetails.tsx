6c93faf1e8f3f9ea3f730549fad4285e98b5e2e5
Jon Simpkins <jonsimpkins@google.com>
2021-09-20 13:19:40 -0500

    :sparkles: allow super basic modification of scrap details (synopsis, prose)

--- src/features/scrapDetails/ScrapDetails.tsx
+++ src/features/scrapDetails/ScrapDetails.tsx
@@ -1,7 +1,11 @@
 import {StoryMap} from '../storyList/storyListSlice';
 import {ScrapMap} from '../scrapList/scrapListSlice';
-import {Component} from 'react';
+import React, {Component, ReactElement} from 'react';
 import {Scrap} from '../../protos_v2';
+import {Breadcrumb, BreadcrumbSection, Form, Segment, TextArea} from 'semantic-ui-react';
+import {
+  Link
+} from 'react-router-dom';
 
 interface ScrapDetailsProps {
   scrapId: string;
@@ -13,6 +17,60 @@ interface ScrapDetailsProps {
 
 export default class ScrapDetails extends Component<ScrapDetailsProps> {
 
+  getBreadcrumbs(thisScrap: Scrap): ReactElement {
+    let parentStories = thisScrap.stories.map((storyId) => {
+      return this.props.storyMap[storyId];
+    }).filter(Boolean);
+
+    return (<div>
+      {
+        parentStories.map((parentStory, idx) => {
+          return (<Breadcrumb>
+            <BreadcrumbSection link>
+              <Link to={'/story/' + parentStory.id}>{parentStory.name}</Link>
+            </BreadcrumbSection>
+          </Breadcrumb>)
+        })
+      }
+    </div>);
+  }
+
+  onSynopsisChange(newSynopsis: string) {
+    const scrap = this.props.scrapMap[this.props.scrapId] as Scrap;
+
+    scrap.synopsis = newSynopsis;
+    this.props.onScrapUpdate(scrap);
+  }
+
+  onProseChange(newProse: string) {
+    const scrap = this.props.scrapMap[this.props.scrapId] as Scrap;
+
+    scrap.prose = newProse;
+    this.props.onScrapUpdate(scrap);
+  }
+
+  getPrimaryForm(thisScrap: Scrap): ReactElement {
+    return <Segment>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Input
+              label='Synopsis'
+              defaultValue={thisScrap.synopsis}
+              onChange={(e) => this.onSynopsisChange(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Field>
+          <label>Prose</label>
+          <TextArea
+              defaultValue={thisScrap.prose}
+              onChange={(e) => this.onProseChange(e.target.value)}
+              style={{fontFamily: 'CourierPrime', height: '400px'}}
+          />
+        </Form.Field>
+      </Form>
+    </Segment>
+  }
+
   render() {
     let thisScrap = this.props.scrapMap[this.props.scrapId];
 
@@ -22,19 +80,10 @@ export default class ScrapDetails extends Component<ScrapDetailsProps> {
       );
     }
 
-    let parentStories = thisScrap.stories.map((storyId) => {
-      return this.props.storyMap[storyId];
-    }).filter(Boolean);
-
     return (
         <div style={{margin: '24px'}}>
-          <p>{thisScrap.synopsis}</p>
-          <p>{thisScrap.prose}</p>
-          <div>
-            {parentStories.map((parentStory) =>
-                (<p>{parentStory.name}</p>)
-            )}
-          </div>
+          {this.getBreadcrumbs(thisScrap)}
+          {this.getPrimaryForm(thisScrap)}
         </div>
     );
   }
